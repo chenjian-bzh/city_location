eb89052835d985b8a58014bb90eb1fe9b4a60dca
cjandcjy <cjandcjy@github.com>
2018-04-10 01:47:12 +0800

    update

--- src/parseExcel.js
+++ src/parseExcel.js
@@ -1,28 +1,49 @@
 let fs = require('fs')
 let xlsx = require('node-xlsx')
-let region = require('../region')
+let region = require('./region')
 
-var obj = xlsx.parse('../地区编码.xlsx');
+var obj = xlsx.parse('code.xlsx');
 var excelObj=obj[0].data;
 
 var data = [];
+let codeObj = {}
+let location = {}
 for(var i in excelObj){
     let obj = {};
     var value=excelObj[i];
-    console.log("### ",value)
-    console.log("--------------------------------")
-    obj = {
-        code:value[0],
-        name:value[1]
-    }
-    data.push(obj);
+    
+    Object.assign(codeObj,{
+        [value[0]]:value[1]
+    })
 }
+region.forEach(item => {
+    Object.assign(location,{
+        [item['name']]:item['geoCoord']
+    })
+})
+console.log("### ",codeObj)
+console.log("--------------------------------")
+let code_locat = {}
+Object.keys(location).forEach(key =>{
+    let loc = location[[key]]
+    let name = ""
+    if(key!='code'){
+        name = codeObj[parseInt([key])]
+    }
+    if(name){
+        Object.assign(code_locat,{
+            "name":name,
+            "value":loc
+        })
+    }
+})
+
 
 // let obj = {cls
-//     "data" : data
+//     "data" : data`
 // }
 
-fs.writeFile("../dist/excel.text",JSON.stringify(data),err => {
+fs.writeFile("../dist/excel.text",JSON.stringify(code_locat),err => {
     if(err){
         console.log(err)
     }else{
